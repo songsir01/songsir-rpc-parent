c48e0d780a8c0a29be1f91804a102daa6e761a96
songsir01 <1126563633@qq.com>
2019-12-14 20:08:35 +0800

    add project model

---
+++ songsir-rpc-common/src/main/java/com/songsir/rpc/constants/SysConstants.java
@@ -0,0 +1,12 @@
+package com.songsir.rpc.constants;
+
+/**
+ * @PackageName com.songsir.rpc.constants
+ * @ProjectName songsir-rpc-parent
+ * @Author: SongYapeng
+ * @Date: Create in 19:59 2019/12/14
+ * @Description:
+ * @Copyright Copyright (c) 2019, songsir01@163.com All Rights Reserved.
+ */
+public class SysConstants {
+}
