ca7e664d7b469c1a596820aa42342b13cb2fc0e0
songsir01 <1126563633@qq.com>
2019-12-25 13:59:13 +0800

    测试接口

---
+++ songsir-rpc-consumer-service/src/main/java/com/songsir/rpc/consumer/service/TestConsumerService.java
@@ -0,0 +1,15 @@
+package com.songsir.rpc.consumer.service;
+
+/**
+ * @PackageName com.songsir.rpc.consumer.service
+ * @ProjectName songsir-rpc-parent
+ * @Author: SongYapeng
+ * @Date: Create in 19:49 2019/12/18
+ * @Description:
+ * @Copyright Copyright (c) 2019, songsir01@163.com All Rights Reserved.
+ */
+public interface TestConsumerService {
+
+    String getHelloWorld(String word);
+
+}
---
+++ songsir-rpc-provider-service/src/main/java/com/songsir/rpc/provider/service/TestProviderService.java
@@ -0,0 +1,23 @@
+package com.songsir.rpc.provider.service;
+
+/**
+ * @PackageName com.songsir.rpc.provider.service
+ * @ProjectName songsir-rpc-parent
+ * @Author: SongYapeng
+ * @Date: Create in 20:29 2019/12/14
+ * @Description:
+ * @Copyright Copyright (c) 2019, songsir01@163.com All Rights Reserved.
+ */
+public interface TestProviderService {
+
+    /**
+     * @MethodName getHelloWorld
+     * @Description 测试
+     * @Author SongYapeng
+     * @Date 2019/12/14 20:29
+     * @param msg
+     * @Since JDK 1.8
+     */
+    String getHelloWorld(String msg);
+
+}
